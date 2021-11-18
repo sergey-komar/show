2038456216e32f3f3d60c61134c2713362eec9a7
sergey-komar <kursev81@yandex.ru>
2021-11-18 22:39:12 +0300

    added block10

--- app/js/main.js
+++ app/js/main.js
@@ -20,6 +20,15 @@ $('.parents-content__popup-link').magnificPopup({
 		fixedContentPos: false
 });
 
+// POPUP BLOCK9
+$('.performances__popup-link').magnificPopup({
+    disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false
+});
 
 // SLIDER BLOCK-2
 	$('.slider__box').slick({
@@ -28,6 +37,15 @@ $('.parents-content__popup-link').magnificPopup({
 		slidesToShow: 3,
   		slidesToScroll: 1,
 		//   centerMode: true,
+		dots: true
+	})
+
+// SLIDER BLOCK 9
+	$('.performances__slider-inner').slick({
+		prevArrow: ' <img src="./images/block-2/arrow-left.png" class="slider__arrow-l" alt="">',
+		nextArrow: '<img src="./images/block-2/arrow-rigth.png" class="slider__arrow-r" alt="">',
+		slidesToShow: 4,
+  		slidesToScroll: 1,
 		dots: true
 	})
 
@@ -52,5 +70,27 @@ $('.parents-content__popup-link').magnificPopup({
 		centerMode: true,
 		focusOnSelect: true
 	  });
+
+// SLIDER BLOCK-10
+$('.super-quests__slider-big').slick({
+	prevArrow: ' <img src="./images/block-2/arrow-left.png" class="slider__arrow-l" alt="">',
+	nextArrow: '<img src="./images/block-2/arrow-rigth.png" class="slider__arrow-r" alt="">',
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	fade: true,
+	asNavFor: '.super-quests__slider-thumb'
+});
+
+$('.super-quests__slider-thumb').slick({
+	
+	slidesToShow: 5,
+	slidesToScroll: 1,
+	asNavFor: '.super-quests__slider-big',
+	dots:false,
+	arrows: false,
+	centerMode: true,
+	focusOnSelect: true
+  });
+
 
 })
